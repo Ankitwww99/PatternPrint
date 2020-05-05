eb6b4d53cdba53d3bebe4631a3fd92fdb0c73aa8
Ankitwww99 <48285443+Ankitwww99@users.noreply.github.com>
2020-05-05 22:06:13 +0530

    binary row triangle

---
+++ src/binary_number.java
@@ -0,0 +1,32 @@
+/*
+10101
+01010
+10101
+01010
+10101
+ */
+import java.util.Scanner;
+
+public class binary_number {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=5;
+        for (int i = 0; i <n ; i++) {
+            for (int j = 0; j <n ; j++) {
+                if(i%2==1){
+                    if(j%2==0)
+                        System.out.print(0);
+                    else
+                        System.out.print(1);
+                }
+                else{
+                    if(j%2==0)
+                        System.out.print(1);
+                    else
+                        System.out.print(0);
+                }
+            }
+            System.out.println();
+        }
+    }
+}
---
+++ src/binary_number_col_triangle.java
@@ -0,0 +1,26 @@
+/*
+1
+10
+101
+1010
+10101
+
+ */
+
+import java.util.Scanner;
+
+public class binary_number_col_triangle {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=5;
+        for (int i = 0; i <n ; i++) {
+            for (int j = 0; j <=i ; j++) {
+                if(j%2==0)
+                    System.out.print(1);
+                else
+                    System.out.print(0);
+            }
+            System.out.println();
+        }
+    }
+}
