f6787a4cda3055158253e08ee26b9aebac25afbb
Ankitwww99 <48285443+Ankitwww99@users.noreply.github.com>
2020-05-05 21:59:34 +0530

    right triangle numeric

--- src/right_triangle_numeric.java
+++ src/right_triangle_numeric.java
@@ -1,22 +1,22 @@
 /*
-5
-5 4
-5 4 3
-5 4 3 2
+1
+2 1
+3 2 1
+4 3 2 1
 5 4 3 2 1
  */
 import java.util.Scanner;
 
-public class descending_number_pattern_col {
+public class right_triangle_numeric {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
         int n=5;
-        for (int i = 5; i >=1 ; i--) {
-            for (int j = 5; j >=i ; j--) {
+        for (int i = 1; i <=5 ; i++) {
+
+            for (int j = i; j >=1 ; j--) {
                 System.out.print(j+" ");
             }
             System.out.println();
         }
     }
-
 }
