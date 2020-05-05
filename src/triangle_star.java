ede36e973a383a88b9c7024c8265628ff3441c94
Ankitwww99 <48285443+Ankitwww99@users.noreply.github.com>
2020-05-05 20:10:11 +0530

    triangle_star

--- src/triangle_star.java
+++ src/triangle_star.java
@@ -1,25 +1,16 @@
 import java.util.Scanner;
 
-public class pyramid_triangle {
+public class triangle_star {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
-        //int n=sc.nextInt();
         int n=5;
-        /*for(int i=0;i<n;i++){
-            for(int j=n;j>i;j--){
-                System.out.print(" ");
-            }
-            for(int j=0;j<=i;j++){
-                System.out.print("* ");
-            }
-            System.out.println();
-        }*/
+
         for(int i=1;i<=n;i++){
             for(int j=n;j>i;j--){
                 System.out.print(" ");
             }
             for(int j=1;j<=2*i-1;j++){
-                if(j%2!=0)
+                if(j==1||j==2*i-1||i==n)
                     System.out.print("*");
                 else
                     System.out.print(" ");
@@ -28,5 +19,5 @@ public class pyramid_triangle {
         }
 
 
-}
+    }
 }
