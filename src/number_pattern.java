43ac92143037bcc4917b8c06841208b5fc6e8009
Ankitwww99 <48285443+Ankitwww99@users.noreply.github.com>
2020-05-05 20:32:56 +0530

    number pattern

--- src/number_pattern.java
+++ src/number_pattern.java
@@ -1,22 +1,29 @@
 /*
 1
-1 2
-1 2 3
-1 2 3 4
-1 2 3 4 5
+2 3
+4 5 6
+7 8 9 10
+11 12 13 14 15
  */
+
+
 
 import java.util.Scanner;
 
-public class simple_number {
+public class number_pattern {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
         int n=5;
+        int k=1;
         for(int i=1;i<=n;i++){
             for(int j=1;j<=i;j++){
-                System.out.print(j+" ");
+                System.out.print(k+" ");
+                k++;
             }
             System.out.println();
         }
+
+
+
     }
 }
